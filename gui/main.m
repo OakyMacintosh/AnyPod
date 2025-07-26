419b0bdaa8cc4b67d5f50b2b452879455b910727
OakyMacintosh <oakymacintosh@duck.com>
2025-07-26 08:50:38 -0300

    Add Guo

---
+++ gui/main.m
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+// @interface Application : NSObject
+
+@implementation Application
+{
+}
+@end
+
+int main() {
+   // main
+
+   return NSApplicationMain;
+}
